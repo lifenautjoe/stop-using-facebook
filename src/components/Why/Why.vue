24cafc422c294132bc96ad0ca9db021d392682a0
Joel Hernandez <joel@open-book.org>
2019-05-08 00:33:48 +0200

    :recycle: move order of why

--- src/components/Why/Why.vue
+++ src/components/Why/Why.vue
@@ -5,7 +5,8 @@
         </h2>
         <ul class="reasons">
             <li v-for="reason in reasons" class="has-text-left reason">
-                <a :href="reason.source" target="_blank" rel="nofollow noopener noreferer" class="is-size-3 is-size-5-mobile reason__link">
+                <a :href="reason.source" target="_blank" rel="nofollow noopener noreferer"
+                   class="is-size-3 is-size-5-mobile reason__link">
                     {{reason.reason}}
                 </a>
             </li>
@@ -72,10 +73,6 @@
             return {
                 'reasons': [
                     {
-                        reason: 'Because Facebook stole 1.5 million people’s email contacts by “mistake” after asking users for their email passwords.',
-                        source: 'https://www.businessinsider.nl/facebook-uploaded-1-5-million-users-email-contacts-without-permission-2019-4/?international=true&r=US'
-                    },
-                    {
                         reason: 'Because Facebook is lobbying against privacy laws, globally.',
                         source: 'https://www.theguardian.com/technology/2019/mar/02/facebook-global-lobbying-campaign-against-data-privacy-laws-investment?CMP=share_btn_tw'
                     },
@@ -92,12 +89,12 @@
                         source: 'https://www.ted.com/talks/carole_cadwalladr_facebook_s_role_in_brexit_and_the_threat_to_democracy?utm_source=twitter.com&utm_medium=social&utm_campaign=tedspread'
                     },
                     {
-                        reason: 'Because Facebook hires climate change deniers as fact-checkers.',
-                        source: '',
+                        reason: 'Because Facebook hired climate change deniers as fact-checkers.',
+                        source: 'https://www.ecowatch.com/facebook-factchecking-science-climate-denial-2635927165.html',
                     },
                     {
-                        reason: 'Because Facebook employees say “F*ck ethics, money is everything”. ',
-                        source: 'https://mashable.com/article/facebook-employees-react-teen-spying-app-blind/',
+                        reason: 'Because Facebook stole 1.5 million people’s email contacts after asking them for their email passwords.',
+                        source: 'https://www.businessinsider.nl/facebook-uploaded-1-5-million-users-email-contacts-without-permission-2019-4/?international=true&r=US'
                     },
                     {
                         reason: 'Because Facebook employees are reading your private messages one by one to train Facebook’s artificial intelligence.',
@@ -108,20 +105,24 @@
                         source: 'https://onezero.medium.com/facebooks-plan-to-fuse-its-messaging-apps-is-not-about-your-privacy-91c237e50c17'
                     },
                     {
-                        reason: 'Because Facebook not only knows conspiracy theories are being spread on the platform, they make a fortune out of it.',
-                        source: 'https://twitter.com/WolfieChristl/status/1058503379377381376',
-                    },
-                    {
-                        reason: 'Because Facebook doesn’t like you knowing reason are you seeing certain ads too much.',
-                        source: 'https://twitter.com/faye_lipson/status/1123145814778494976'
-                    },
-                    {
                         reason: 'Because Facebook updated an old press release with information that millions of passwords of instagram accounts had been compromised while the Mueller report was being released in order to avoid criticism and scrutiny.',
                         source: 'https://edition.cnn.com/2019/04/18/tech/facebook-news-dump/index.html'
                     },
                     {
                         reason: 'Because Facebook hired the author of the PATRIOT act, the most significant threats to civil liberties, privacy, and democratic traditions in US history, as general counsel.',
                         source: 'https://observer.com/2019/04/facebook-jennifer-newstead-general-counsel-patriot-act/\n',
+                    },
+                    {
+                        reason: 'Because Facebook employees say “F*ck ethics, money is everything”. ',
+                        source: 'https://mashable.com/article/facebook-employees-react-teen-spying-app-blind/',
+                    },
+                    {
+                        reason: 'Because Facebook not only knows conspiracy theories are being spread on the platform, they make a fortune out of it.',
+                        source: 'https://twitter.com/WolfieChristl/status/1058503379377381376',
+                    },
+                    {
+                        reason: 'Because Facebook doesn’t want you to know the reason you see certain ads, just pretends it does.',
+                        source: 'https://twitter.com/faye_lipson/status/1123145814778494976'
                     },
                 ]
             }
