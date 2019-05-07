e737fa550d452e7a32228258a47821bf3380f852
Joel Hernandez <joel@open-book.org>
2019-05-07 17:41:48 +0200

    :bug: fix mobile header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,6 @@
 <template>
     <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
         <div class="container">
-            <div class="navbar-brand">
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
             <div class="navbar-menu" id="navbarBasicExample">
                 <div class="navbar-end">
                     <a class="navbar-item" href="#what" v-scroll-to="'#what'">
@@ -26,6 +19,15 @@
 </template>
 
 <style lang="scss">
+    .navbar-menu{
+        box-shadow: none !important;
+        display: flex !important;
+        flex-direction: row-reverse;
+
+        .navbar-end{
+            display: flex !important;
+        }
+    }
 </style>
 
 <script>
