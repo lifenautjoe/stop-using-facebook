ad9acb1ed9407a5e10e5d3fdff987ffc3f4520ca
Joel Hernandez <joel@open-book.org>
2019-05-07 15:34:34 +0200

    :sparkles: add messaging apps

--- src/components/How/How.vue
+++ src/components/How/How.vue
@@ -6,17 +6,22 @@
         <div class="hows">
             <div v-for="how in hows" class="how has-text-left">
                 <h4 class="is-size-3 how-title">
-                    Replace
+                    {{how.replace}}
                 </h4>
                 <div class="columns is-mobile is-multiline">
-                    <sf-product class="column is-6-mobile is-3-tablet is-3-desktop" v-for="item in how.what" :name="item.name" :image="item.image" :url="item.url" v-bind:key="item.name"></sf-product>
+                    <sf-product class="column is-6-mobile is-3-tablet is-3-desktop" v-for="item in how.what"
+                                :name="item.name" :image="item.image" :url="item.url"
+                                v-bind:key="item.name"></sf-product>
                 </div>
                 <img :src="how.what.image" :alt="how.what.name" class="how-what__img">
                 <h4 class="is-size-3 how-title">
-                    With
+                    with
                 </h4>
                 <div class="columns is-mobile is-multiline">
-                    <sf-product class="column is-12-mobile is-4-tablet is-4-desktop how-replace-with" v-for="alternative in how.alternatives" :name="alternative.name" :image="alternative.image" :url="alternative.url"  :description="alternative.description" v-bind:key="alternative.name"></sf-product>
+                    <sf-product class="column is-12-mobile is-4-tablet is-4-desktop how-replace-with"
+                                v-for="alternative in how.alternatives" :name="alternative.name"
+                                :image="alternative.image" :url="alternative.url" :description="alternative.description"
+                                v-bind:key="alternative.name"></sf-product>
                 </div>
             </div>
         </div>
@@ -38,24 +43,30 @@
         max-width: 800px;
         padding: 1rem;
 
-        &-title{
+        &-title {
             padding-bottom: 1em;
         }
 
-        &-what{
-            &__img{
+        &-what {
+            &__img {
                 max-width: 400px;
             }
         }
 
-        &-replace-with{
+        &-replace-with {
             min-height: 300px;
         }
     }
 </style>
 
 <script>
+    import signalLogo from './assets/signal-300x300.png';
+    import telegramLogo from './assets/Telegram-300x300.png';
+    import riotLogo from './assets/riotim-300x300.jpg';
+    import zomLogo from './assets/zom-300x300.png';
     import fbLogo from './assets/fb_logo.png';
+    import waLogo from './assets/wa_logo.png';
+    import messengerLogo from './assets/messenger.png';
     import instagramLogo from './assets/instagram_logo.png';
     import mastodonLogo from './assets/mastodon-300x300.png';
     import friendicaLogo from './assets/friendica-300x300.png';
@@ -77,6 +88,7 @@
             return {
                 'hows': [
                     {
+                        replace: 'Replace the social apps',
                         what: [
                             {
                                 name: 'Facebook',
@@ -121,18 +133,6 @@
                                 url: 'https://steemit.com/'
                             },
                             {
-                                name: 'Scuttlebutt',
-                                description: 'Named after "basically a watercooler on a ship", this app is for gossiping securely',
-                                image: scuttle,
-                                url: 'https://www.scuttlebutt.nz/'
-                            },
-                            {
-                                name: 'Zap',
-                                description: 'Social network with privacy centre stage',
-                                image: zap,
-                                url: 'https://zotlabs.com/zap/'
-                            },
-                            {
                                 name: 'Openbook',
                                 description: 'Privacy-friendly, transparent and community-driven social network.',
                                 image: openbookLogo,
@@ -144,14 +144,42 @@
                                 image: tookapicLogo,
                                 url: 'https://tookapic.com/'
                             },
+                        ],
+                    },
+                    {
+                        replace: 'Replace the messaging apps',
+                        what: [
                             {
-                                name: 'Micro.blog',
-                                description: 'A safe community for micro blogs.',
-                                image: Microblog,
-                                url: 'https://micro.blog/'
+                                name: 'Whatsapp',
+                                image: waLogo,
+                                url: 'https://whatsapp.com',
                             },
-
-                        ]
+                            {
+                                name: 'Messenger',
+                                image: messengerLogo,
+                                url: 'https://messenger.com',
+                            },
+                        ],
+                        alternatives: [
+                            {
+                                name: 'Signal',
+                                description: 'Open-source text, voice and video messaging app supported entirely by grants and donations',
+                                image: signalLogo,
+                                url: 'https://signal.org/'
+                            },
+                            {
+                                name: 'Telegram',
+                                description: 'Fast and secure cloud-based messaging',
+                                image: telegramLogo,
+                                url: 'https://telegram.org/'
+                            },
+                            {
+                                name: 'Zom',
+                                description: 'Open-source messenger app created by a group of friends with an interest in Tibetan culture',
+                                image: zomLogo,
+                                url: 'https://zom.im/'
+                            },
+                        ],
                     },
                 ]
             }
